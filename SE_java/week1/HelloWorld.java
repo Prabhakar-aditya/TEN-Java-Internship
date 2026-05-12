f0a8b45bd08af2acfe590dfbb7cb13a0ddb38485
Prabhakar panchal <p8711683@gmail.com>
2026-05-12 15:54:20 +0530

    Add files via upload

---
+++ SE_java/week1/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        String name = "Prabhakar";
+        int age = 18;
+        double cgpa = 8.5;
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Student: " + isStudent);
+    }
+}
---
+++ SE_java/week1/StudentInfo.java
@@ -0,0 +1,14 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "Prabhakar Panchal";
+        String branch = "Cyber Security";
+        String college = "GITAM University";
+        int year = 4;
+        String language = "Java";
+        System.out.println("Student Name: " + name);
+        System.out.println("Branch: " + branch);
+        System.out.println("College: " + college);
+        System.out.println("Year: " + year);
+        System.out.println("Favorite Language: " + language);
+    }
+}
